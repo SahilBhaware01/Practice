a49d8737d2f0b7c11beeb791c8fd0933cdbfc696
sahil bhaware <sahilbhaware01@gmail.com>
2021-01-08 19:33:18 +0530

    created student form

---
+++ pages/icons.vue
@@ -0,0 +1,339 @@
+<template>
+  <div>
+  <h3>Personal Details</h3>
+   <b-container fluid>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Full Name:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your name"></b-form-input>
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Email:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input  type="email" id="input-default" placeholder="Enter your email"></b-form-input>
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Mobile:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input  type="tel" id="input-default" placeholder="Enter your mobile number"></b-form-input>
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Address:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input id="input-default" type="text" placeholder="Enter your address"></b-form-input>
+    </b-col>
+  </b-row>
+
+<hr>
+<h3>Post Gradutation</h3>
+<b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default"> College:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your college name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default"> Department:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your department name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation complete date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Percentage:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="number" id="input-default" placeholder="Enter gradutation percentage"></b-form-input>
+    </b-col>
+  </b-row>
+  <hr>
+
+<h3>Gradutation</h3>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default"> College:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your college name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default"> Department:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your department name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation complete date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Percentage:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="number" id="input-default" placeholder="Enter gradutation percentage"></b-form-input>
+    </b-col>
+  </b-row>
+
+  <hr>
+
+<h3>Higher Secondary Education</h3>
+<b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">School:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your college name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Board:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your board name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation complete date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Percentage:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="number" id="input-default" placeholder="Enter gradutation percentage"></b-form-input>
+    </b-col>
+  </b-row>
+
+     
+     <hr>
+
+<h3>Secondary Education</h3>
+<b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default"> College:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your college name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Board:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your board name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Gradutation complete date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Percentage:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="number" id="input-default" placeholder="Enter gradutation percentage"></b-form-input>
+    </b-col>
+  </b-row>
+  </b-container>
+  
+  <hr>
+  
+  <b-container fluid>
+
+  </b-container>
+  
+  <hr>
+  
+  <b-container fluid>
+   <h3>Internship Details</h3>
+<b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Intern Profile:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your profile"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Organisation:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter organisation name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Location:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter location"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Internship start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Internship completion date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Description:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter internship description"></b-form-input>
+    </b-col>
+  </b-row>
+  </b-container>
+
+  <hr>
+
+  <b-container fluid>
+     <h3>Job Details</h3>
+<b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Job Profile:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter your job profile"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Organisation:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter organisation name"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Location:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter Job location"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Start Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Job start date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Finish Date:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="date" id="input-default" placeholder="Job completion date"></b-form-input>
+    </b-col>
+  </b-row>
+  <b-row class="my-1">
+    <b-col sm="2">
+      <label for="input-default">Description:</label>
+    </b-col>
+    <b-col sm="8">
+      <b-form-input type="text" id="input-default" placeholder="Enter job description"></b-form-input>
+    </b-col>
+  </b-row>
+  </b-container>
+
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'icons'
+};
+</script>
+<style></style>
